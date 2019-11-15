02a9bacd995f7fbca9af5cc0bf0621ee9f6e075f
Alex Bonwit <alexbonwit@mac.com>
2019-11-15 23:13:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,14 +12,14 @@ def nyc_pigeon_organizer(data)
         if !pigeon_hash[string] 
         pigeon_hash[string] = {}
         end
-          binding.pry
+        binding.pry
       end
       
     end
   end
-  pigeon_hash.each do |key, value|
-    #if data[key][key][value].include? 'pigeon_hash[key]' then pigeon_hash[key] = value
-    pigeon_hash[key] = 
+  # pigeon_hash.each do |key, value|
+  #   #if data[key][key][value].include? 'pigeon_hash[key]' then pigeon_hash[key] = value
+  #   pigeon_hash[key] = 
   end
   
 end
